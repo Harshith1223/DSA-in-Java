173e4169456ba2ce725b216ca15e14c49a1a6569
Harshith1223 <harshith.hrithik001@gmail.com>
2024-04-04 06:38:28 +0530

    Variables,I/O

---
+++ pattern.java
@@ -0,0 +1,13 @@
+class pattern{
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+//This program is for practice of printing
