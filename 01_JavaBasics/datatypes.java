20d7199dfb61b02ba2a7d6ac8cf2c5eceabcc5ec
Harshith1223 <harshith.hrithik001@gmail.com>
2024-04-12 07:01:47 +0530

    "constructors"

---
+++ 01_JavaBasics/datatypes.java
@@ -0,0 +1,14 @@
+public class datatypes {
+    public static void main(String[] main)
+    {
+        byte a=123;
+        short b=1573;
+        int c=1647;
+        long d=17384;
+        float e=1938.948f;
+        double f=8714627.98247d;
+        char g='X';
+        boolean h=true;
+        System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h);
+    }
+}
