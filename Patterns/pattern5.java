7654b64e85977f41dce1d362a458ecbe458b3209
Harshith1223 <harshith.hrithik001@gmail.com>
2024-04-19 06:32:45 +0530

    "Patterns"

---
+++ Patterns/pattern5.java
@@ -0,0 +1,15 @@
+class pattern5
+{
+    public static void main(String[] args) {
+        int n=4;
+        for (int i=1;i<=n;i++){
+            //inner loop for printig spaces
+          for (int j=1;j<=n-i;j++)
+                System.out.print(" ");
+
+            for    j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }/*-+
+}
