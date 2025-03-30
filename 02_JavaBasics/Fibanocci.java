372ccb94f33feed3970006c76216cbd6f64c40f6
Harshith1223 <harshith.hrithik001@gmail.com>
2025-03-30 21:29:07 +0530

    changes

---
+++ 02_JavaBasics/Fibanocci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fibanocci {
+    public static void main(String[] args) {
+        int fib1=0;
+        int fib2=1;
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.print(fib1 + " " + fib2 + " ");
+        for (int i = 0; i < input; i++) {
+            int fib3 = fib1+fib2;
+            System.out.print(fib3+" ");
+            fib1=fib2;
+            fib2 = fib3;
+        }
+    }
+}
